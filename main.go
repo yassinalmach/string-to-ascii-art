0485a23ebb41cc490daab4bdcfdd7cfa626cd24c
yelmach <yassineelmach0@gmail.com>
2024-11-04 18:42:03 +0100

    main function

--- main.go
+++ main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"AsciiArt/utils"
-	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	flags := utils.CheckArgs(os.Args)
-	fmt.Println(flags)
+
+	asciiMap, err := utils.MakeMap(flags.BannerFile)
+	if err != nil {
+		utils.PrintError("banner")
+	}
+
+	// Split the string string into lines
+	splittedStr := utils.SplitString(flags.Str, flags.AlignOption)
+
+	// Make the Ascii Art
+	result := utils.GenerateArt(asciiMap, splittedStr, flags)
+
+	// Print the Ascii Art
+	err = utils.WriteOutput(result, flags.OutputFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
