a2d124e0d1dd80bdba60c98def51c9e04189ca0f
yassinalmach <yassineelmach0@gmail.com>
2024-11-08 18:10:17 +0100

    fixing reverse functionality

--- utils/reverse.go
+++ utils/reverse.go
@@ -1,52 +1,46 @@
 package utils
 
-import "log"
-
 var rev string
 
-func Reverse(inputFile []string, asciiMap map[rune][]string, idx int) string {
-	for i := idx; i < len(inputFile); i++ {
-		if inputFile[idx] == "" {
-			rev += "\n"
-			i++
-			Reverse(inputFile, asciiMap, i)
-		}
+func Reverse(inputFile []string, asciiMap map[rune][]string) string {
+    for i := 0; i < len(inputFile); i++ {
+        if inputFile[i] == "" && i != len(inputFile)-1{
+            rev += "\n"
+            continue
+        }
 
-		if len(inputFile[i:]) >= 8 {
-			for char := ' '; char <= '~'; char++ {
-				charWidth := len(asciiMap[char][0])
-				if isMatched(asciiMap[char], inputFile[i:i+9], charWidth) {
-					newInputFile := removeChar(charWidth, inputFile[i:i+9])
-					rev += string(char)
-					Reverse(newInputFile, asciiMap, i)
-				}
-			}
-			if len(inputFile) > 8 {
-				i += 8
-				rev += "\n"
-				Reverse(inputFile, asciiMap, i)
-			}
-		} else {
-			log.Fatalln("error file format incorrect")
-		}
-		i += 7
-	}
-	return rev
+        if len(inputFile[i:]) >= 8 {
+            for inputFile[i] != "" {
+                for char := ' '; char <= '~'; char++ {
+                    charWidth := len(asciiMap[char][0])
+                    if isMatched(asciiMap[char], inputFile[i:], charWidth) {
+                        rev += string(char)
+                        inputFile = removeChar(charWidth, inputFile, i)
+                    }
+                }
+            }
+            i += 7
+            rev += "\n"
+        }
+    }
+    return rev[:len(rev)-1]
 }
 
 func isMatched(asciiChar, inputFile []string, charWidth int) bool {
-	for i := 0; i < 8; i++ {
-		if len(inputFile[i]) < len(asciiChar[i]) || asciiChar[i] != inputFile[i][:charWidth] {
-			return false
-		}
-	}
-	return true
+    for i := 0; i < 8; i++ {
+        if len(inputFile[i]) < len(asciiChar[i]) || asciiChar[i] != inputFile[i][:charWidth] {
+            return false
+        }
+    }
+    return true
 }
 
-func removeChar(width int, inputFile []string) []string {
-	newInputFile := []string{}
-	for i := 0; i < 8; i++ {
-		newInputFile = append(newInputFile, inputFile[i][width:])
-	}
-	return newInputFile
+func removeChar(width int, inputFile []string, idx int) []string {
+    newInputFile := []string{}
+    newInputFile = append(newInputFile, inputFile[:idx]...)
+    for i := idx; i < idx+8; i++ {
+        newInputFile = append(newInputFile, inputFile[i][width:])
+    }
+    newInputFile = append(newInputFile, inputFile[idx+8:]...)
+    return newInputFile
 }
